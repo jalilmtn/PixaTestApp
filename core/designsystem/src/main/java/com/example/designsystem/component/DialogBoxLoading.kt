88ad753a301459a774b40acf8f1b7723b5fec1bf
Jalil <taghinezhad.jalil@yahoo.com>
2023-01-17 19:39:32 +0300

    code clean

--- core/designsystem/src/main/java/com/example/designsystem/component/DialogBoxLoading.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/DialogBoxLoading.kt
@@ -9,8 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
