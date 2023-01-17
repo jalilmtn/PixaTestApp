3d027ce382114d32ba98dbb1b3f7528850ead9b1
Jalil <taghinezhad.jalil@yahoo.com>
2023-01-17 14:28:47 +0300

    change lifecycle

--- core/database/src/main/java/com/example/database/data/ImageDatabaseRepoImpl.kt
+++ core/database/src/main/java/com/example/database/data/ImageDatabaseRepoImpl.kt
@@ -4,35 +4,41 @@ import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
 import imagedb.ImageEntitiy
 import imagedb.ImageEntitiyQueries
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ImageDatabaseRepoImpl @Inject constructor(
     private val imageDataSource: ImageEntitiyQueries,
 ) : ImageDatabaseRepo {
     override suspend fun getImagesByTag(localTag: String): Flow<List<ImageEntitiy>> {
-        return imageDataSource.getImagesByTag(localTag).asFlow().mapToList()
+        return withContext(Dispatchers.IO) {
+            imageDataSource.getImagesByTag(localTag).asFlow().mapToList()
+        }
     }
 
     override suspend fun getImageById(id: Long): ImageEntitiy? {
-        return imageDataSource.getImageById(id).executeAsOneOrNull()
+        return withContext(Dispatchers.IO) { imageDataSource.getImageById(id).executeAsOneOrNull() }
     }
 
     override suspend fun insertImage(localTag: String, image: ImageEntitiy) {
-        with(image) {
-            imageDataSource.insertImage(
-                id = id,
-                user = user,
-                largeImageURL = largeImageURL,
-                comments = comments,
-                downloads = downloads,
-                likes = likes,
-                previewURL = previewURL,
-                tags = tags,
-                localTag = localTag,
-                previewHeight = previewHeight,
-                previewWidth = previewWidth
-            )
+        withContext(Dispatchers.IO) {
+            with(image) {
+                imageDataSource.insertImage(
+                    id = id,
+                    user = user,
+                    largeImageURL = largeImageURL,
+                    comments = comments,
+                    downloads = downloads,
+                    likes = likes,
+                    previewURL = previewURL,
+                    tags = tags,
+                    localTag = localTag,
+                    previewHeight = previewHeight,
+                    previewWidth = previewWidth
+                )
+            }
         }
     }
 }
